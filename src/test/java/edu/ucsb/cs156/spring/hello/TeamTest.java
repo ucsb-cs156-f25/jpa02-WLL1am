ffc3bb6cf84b48c793a7fb32c66631ac77da37a3
William Chung <118134309+WLL1am@users.noreply.github.com>
2025-10-07 23:55:47 -0700

    wc - added test cases for equals and hashCode()

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -1,6 +1,11 @@
 package edu.ucsb.cs156.spring.hello;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import java.beans.Transient;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -22,5 +27,84 @@ public class TeamTest {
    
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+
+    @Test
+    public void equals_same_object() {
+        Team t = new Team();
+        t.setName("testName");
+        t.addMember("testMember");
+        assertTrue(t.equals(t));
+    }
+
+    @Test
+    public void equals_different_class() {
+        Team t = new Team();
+        t.setName("testName");
+        t.addMember("testMember");
+        String str = "testString";
+        assertFalse(t.equals(str));
+    }
+
+    @Test
+    public void equals_same_name_and_members() {
+        Team t1 = new Team();
+        t1.setName("A");
+        t1.addMember("Alice");
+        t1.addMember("Bob");
+        Team t2 = new Team();
+        t2.setName("A");
+        t2.addMember("Alice");
+        t2.addMember("Bob");
+        assertTrue(t1.equals(t2));
+    }
+
+    @Test
+    public void equals_same_name_diff_members() {
+        Team t1 = new Team();
+        t1.setName("A");
+        t1.addMember("Alice");
+        t1.addMember("Bob");
+        Team t2 = new Team();
+        t2.setName("B");
+        t2.addMember("Alice");
+        t2.addMember("Bob");
+        assertFalse(t1.equals(t2));
+    }
+
+    // @Test
+    // public void equals_diff_name_same_members() {
+
+    // }
+
+    // @Test public void
+    // testEqualsSameNameSameMembers() {
+    //     Team team1 = new Team("A", List.of("Alice", "Bob"));
+    //     Team team2 = new Team("A", List.of("Alice", "Bob"));
+    //     assertTrue(team1.equals(team2));
+    // }
+
+
+    @Test
+    public void hashCode_returns_same_value() {
+        Team t1 = new Team();
+        t1.setName("foo");
+        t1.addMember("bar");
+        Team t2 = new Team();
+        t2.setName("foo");
+        t2.addMember("bar");
+        assertEquals(t1.hashCode(), t2.hashCode());
+    }
+
+    @Test
+    public void hashCode_returns_correct_value() {
+        Team t = new Team();
+        int result = t.hashCode();
+        int expectedResult = 1;
+        assertEquals(expectedResult, result);
+    }
 
 }
